3bb11abc74963ad8d97dd30198f28cd0e7601021
Adil Geelani <87556416+AdilGeelani@users.noreply.github.com>
2022-07-27 15:36:04 +0530

    Delete innomatics_internship_july_2022/python_task/Task_2 directory

--- innomatics_internship_july_2022/python_task/Task_2/9.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-m = int(input())
-a = set(input().split())
-n = int(input())
-b = set(input().split())
-inter = a.intersection(b)
-u = a.union(b)
-
-print ('\n'.join(sorted(u.difference(inter), key=int)))
