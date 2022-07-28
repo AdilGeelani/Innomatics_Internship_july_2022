38c0fef7e276416279e4b392b536f648a6c3fac6
Adil Geelani <87556416+AdilGeelani@users.noreply.github.com>
2022-07-28 13:04:28 +0530

    Delete innomatics_internship_july_2022/python_task/Task_3 directory

--- innomatics_internship_july_2022/python_task/Task_3/1.py
+++
@@ -1,5 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from cmath import phase
-z = complex(input())
-print(abs(complex(z.real , z.imag)))
-print(phase(complex(z.real, z.imag)))
--- innomatics_internship_july_2022/python_task/Task_3/2.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import math
-ab = int(input())
-bc = int(input())
-ac = math.sqrt(ab**2 * bc**2)
-mc = ac/2
-bca =  math.atan(ab/bc)
-# the property of right angle triange(median to hypotenuses divide it into 2 isoceles triangles soe am = bm = cm ) therefore angle acb = mbc
-print(str(int(round(math.degrees(bca)))) + u"\N{DEGREE SIGN}")
